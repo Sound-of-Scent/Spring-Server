2ee6b38579873df3b7a0d99484dd3fccf2033186
saeyeonn <sy01lim@naver.com>
2025-03-23 15:32:24 +0900

    :sparkles: Feat: Configuration 파일 생성

---
+++ src/main/java/com/scent/perfume/config/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package com.scent.perfume.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSocketConfig {
+}
---
+++ src/main/java/com/scent/perfume/security/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.scent.perfume.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
